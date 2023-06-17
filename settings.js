9b856d4359c3e12722e1bfe5f00ae513f4cdb09b
KiraiiSenpai <111110504+KiraiiSenpai@users.noreply.github.com>
2023-06-17 12:48:54 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,27 +54,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "𝙑𝙚𝙡𝙙𝙤𝙧𝙖-Mdོ" //namabot kalian
-global.ownername = " Diablo Tempest '×፝֟͜×" //nama kalian
-global.myweb = "https://s.id//diablooctagram" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@diablooctagram" //bebas asal jan hapus
-global.myweb2 = "https://s.id/diablooctagram" //bebas
+global.botname = "𝙑𝙚𝙡𝙙𝙤𝙧𝙖-𝙈𝘿⚡" //namabot kalian
+global.ownername = "𝕯𝖎𝖆𝖇𝖑𝖔 ×፝֟͜×" //nama kalian
+global.myweb = "https://s.id/diablosoctagram" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@diablosoctagram" //bebas asal jan hapus
+global.myweb2 = "https://s.id/diablosoctagram" //bebas
 global.email = "diablosoctagram@gmail.com" //bebas
-global.region = "Malaysia" //bebas
+global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.badword_list = ["asu", "anjir"] //tambahin sendiri kata² nya, gunakan huruf kecil
-global.premium = ['60146144525'] //premium user
-global.owner = ["601137759884", "60146144525"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = owner[0] + "@s.whatsapp.net"
-global.ownernomerr = "+"+ownernomer //tidak perlu di edit
+global.premium = ['60146144525','601137759884'] //premium user
+global.owner = ["60146144525"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60146144525" // nomor wa kalian
+global.ownernomerr = "60146144525" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.tag_owner = "./tag_owner.mp3" // audio ini akan di kirim apabila ada member yang menandai (tag) owner bot 
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
 global.packname = '© 𝕍𝕖𝕝𝕕𝕠𝕣𝕒𝕄𝔻' //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.author = 'Created By 𝔻𝕚𝕒𝕓𝕝𝕠 𝕏' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
 //type menu for menu
